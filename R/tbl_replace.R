1dd92858a8aec084f23601e79d7c386e580f9990
Chirag <chirag.parsania@gmail.com>
2021-08-30 15:48:58 +1000

    tbl_replace_numeric added

--- R/tbl_replace.R
+++ R/tbl_replace.R
@@ -134,3 +134,28 @@ tbl_replace_string <- function(tbl, pattern, replacement){
                                                                                                     replacement = !!..3)))
         tbl %>% mm(pattern , replacement)
 }
+
+#' @title  Replace numeric value in a tibble.
+#' @description In a given tibble a numeric value can be replaced by another numeric value.
+#' @param tbl a tbl.
+#' @param value a numeric which is to be replaced from \code{tbl}.
+#' @param replace_by a numeric replacement for \code{value}.
+#' @return a tbl
+#' @export
+#' \dontrun{
+#' set.seed(1234)
+#' tbl <- tibble::tibble(x = c(letters[1:5] ,letters[1:5]),
+#'                        y = sample(1:5, 10 ,replace = T),
+#'                        z = sample(1:5, 10,replace = T))
+#'  tbl %>% tbl_replace_numeric(2, 1000)
+#'  tbl %>% tbl_replace_numeric(4, 1000)
+#' }
+tbl_replace_numeric <- function(tbl, value , replace_by){
+
+        if ( !is_tibble(tbl)  ) stop("tbl is not tbl")
+        stopifnot(is.numeric(value))
+        stopifnot(is.numeric(replace_by))
+
+        mm <- purrr::as_mapper(~ ..1 %>% dplyr::mutate_if(is.numeric , ~ dplyr::if_else(. == !!..2 , !!..3 , as.double(.) )))
+        tbl %>% mm(value , replace_by)
+}
